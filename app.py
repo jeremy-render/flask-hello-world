438d1b17595bde115d956e587d02c91a6c94b051
Jérémy Faret <jeremy@render.com>
2023-07-17 12:02:25 +0300

    Time print

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from datetime import datetime
+
 app = Flask(__name__)
+
+start_time = datetime.now()  # gets the current time when the application starts
 
 @app.route('/')
 def hello_world():
-    return 'Hello Render! This is an update again'
+    return f'Hello Render! This app was started at {start_time}.'
